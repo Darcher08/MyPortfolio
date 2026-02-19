8fc0e8c8b14becdff3ff0f6ced0842f9e824780e
Darcher08 <148842609+Darcher08@users.noreply.github.com>
2026-02-19 07:34:53 -0600

    Add astro-icon, assets and UI components
    Integrate astro-icon into the Astro config and add support for icons. Add a collection of tech/logo images and SVG icons under public/img, introduce new components (Aptitude.astro, Technologies.astro) and update existing components (About.astro, Introduce.astro). Also update global styles and Tailwind config, and update package metadata/dependencies accordingly to enable the new icon/asset usage.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
   plugins: [],
-}
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee 50s linear infinite",
+        "marquee-reverse": "marquee-reverse 45s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" }, // -50% porque duplicaste los items
+        },
+        "marquee-reverse": {
+          "0%": { transform: "translateX(-50%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
+};
